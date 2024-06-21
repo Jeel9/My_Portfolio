4f16757fa2a27946045a31782a93c5a06c9e5ce4
Jeel <99079547+Jeel9@users.noreply.github.com>
2024-06-21 16:51:33 +0530

    Fixes in Tech section

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -8,7 +8,7 @@ export function CanvasRevealEffectDemo(props) {
     var icons = [];
     return (
         <>
-            <div className="py-[40px] flex items-center justify-center bg-white dark:bg-[#0e0e0e] w-full gap-[8px] mx-auto px-8">
+            <div className="py-[40px] flex items-center justify-center bg-[#0e0e0e] w-full gap-[8px] mx-auto px-8">
                 <Card title="Web Dev" icon={props.weblist}>
                     <CanvasRevealEffect
                         animationSpeed={4.5}
@@ -20,7 +20,7 @@ export function CanvasRevealEffectDemo(props) {
                         dotSize={2}
                     />
                     {/* Radial gradient for the cute fade */}
-                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/90" />
                 </Card>
                 <Card title="Mobile Apps" icon={props.applist}>
                     <CanvasRevealEffect
@@ -33,7 +33,7 @@ export function CanvasRevealEffectDemo(props) {
                         dotSize={2}
                     />
                     {/* Radial gradient for the cute fade */}
-                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/90" />
                 </Card>
                 <Card title="UI/UX Design" icon={props.dsglist}>
                     <CanvasRevealEffect
@@ -46,7 +46,7 @@ export function CanvasRevealEffectDemo(props) {
                         dotSize={2}
                     />
                     {/* Radial gradient for the cute fade */}
-                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+                    <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/90" />
                 </Card>
             </div>
         </>
@@ -67,7 +67,7 @@ const Card = ({
         <div
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
-            className=" border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full p-4 h-[30rem] relative overflow-hidden"
+            className=" border border-white/[0.1] group/canvas-card flex items-center justify-center  max-w-sm w-full p-4 h-[30rem] relative overflow-hidden"
         >
             {/* <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
             <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -102,41 +102,41 @@ const Card = ({
     );
 };
 
-const AceternityIcon = () => {
-    return (
-        <svg
-            width="66"
-            height="65"
-            viewBox="0 0 66 65"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-        >
-            <path
-                d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-                stroke="currentColor"
-                strokeWidth="15"
-                strokeMiterlimit="3.86874"
-                strokeLinecap="round"
-                style={{ mixBlendMode: "darken" }}
-            />
-        </svg>
-    );
-};
+// const AceternityIcon = () => {
+//     return (
+//         <svg
+//             width="66"
+//             height="65"
+//             viewBox="0 0 66 65"
+//             fill="none"
+//             xmlns="http://www.w3.org/2000/svg"
+//             className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
+//         >
+//             <path
+//                 d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
+//                 stroke="currentColor"
+//                 strokeWidth="15"
+//                 strokeMiterlimit="3.86874"
+//                 strokeLinecap="round"
+//                 style={{ mixBlendMode: "darken" }}
+//             />
+//         </svg>
+//     );
+// };
 
-export const Icon = ({ className, ...rest }: any) => {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className={className}
-            {...rest}
-        >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
-        </svg>
-    );
-};
+// export const Icon = ({ className, ...rest }: any) => {
+//     return (
+//         <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             fill="none"
+//             viewBox="0 0 24 24"
+//             strokeWidth="1.5"
+//             stroke="currentColor"
+//             className={className}
+//             {...rest}
+//         >
+//             <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
+//         </svg>
+//     );
+// };
 
